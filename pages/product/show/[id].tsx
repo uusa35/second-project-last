fed804032fef1109e7b7c91dd62f6fde49dada10
uusa35 <uusa35@gmail.com>
2023-01-03 17:08:06 +0300

    prepare the project structure and queries

--- pages/product/show/[id].tsx
+++ pages/product/show/[id].tsx
@@ -1,7 +1,16 @@
 import MainContentLayout from '@/layouts/MainContentLayout';
 import { wrapper } from '@/redux/store';
+import { AppQueryResult, Category } from '@/types/queries';
+import { categoryApi } from '@/redux/api/categoryApi';
+import { apiSlice } from '@/redux/api';
+import { Product } from '@/types/index';
+import { productApi } from '@/redux/api/productApi';
+import { NextPage } from 'next';
 
-const ProductShow = () => {
+type Props = {
+  element: Product;
+};
+const ProductShow: NextPage<Props> = ({ element }) => {
   return (
     <MainContentLayout>
       <div>ProductShow</div>
@@ -12,9 +21,32 @@ const ProductShow = () => {
 export default ProductShow;
 
 export const getServerSideProps = wrapper.getServerSideProps(
-  (store) => async () => {
-    return {
-      props: {},
-    };
-  }
+  (store) =>
+    async ({ query }) => {
+      const { id }: any = query;
+      if (id) {
+        return {
+          notFound: false,
+        };
+      }
+      const {
+        data: element,
+        isError,
+      }: {
+        data: AppQueryResult<Product>;
+        isError: boolean;
+      } = await store.dispatch(productApi.endpoints.getProduct.initiate(id));
+
+      await Promise.all(store.dispatch(apiSlice.util.getRunningQueriesThunk()));
+      if (isError || !element.status || !element.Data) {
+        return {
+          notFound: true,
+        };
+      }
+      return {
+        props: {
+          element: element.Data,
+        },
+      };
+    }
 );
