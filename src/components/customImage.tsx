6d4cbf92760a8693fa8603fd1969009d04bd3d52
eren <e.atef@unityco.io>
2023-01-04 11:06:48 +0200

    fallback image fix rest params

--- src/components/customImage.tsx
+++ src/components/customImage.tsx
@@ -1,26 +1,33 @@
 import Image from 'next/image';
 import React, { FC, useEffect, useState } from 'react';
 
-type Props={
-src:string,
-alt:string
-}
+type Props = {
+  src: string;
+  alt: string;
+  [x:string]: any;
+};
 
-const customImage: FC<Props>=({src='',alt='',...rest}):JSX.Element=>{
-    const [error, setError] = useState<boolean>(false)
+const CustomImage: FC<Props> = ({
+  src = '',
+  alt = '',
+  ...rest
+}): JSX.Element => {
+  const [error, setError] = useState<boolean>(false);
 
-    useEffect(() => {
-      setError(false)
-    }, [src])
+  useEffect(() => {
+    setError(false);
+  }, [src]);
 
-    return(
-        <Image
-        {...rest}
-        alt={alt}
-        src={error ? '': src}
-        onError={()=>{setError(true)}}
-        />
-    )
+  return (
+    <Image
+      {...rest}
+      alt={alt}
+      src={error ? '' : src}
+      onError={() => {
+        setError(true);
+      }}
+    />
+  );
+};
 
-}
-
+export default CustomImage;
