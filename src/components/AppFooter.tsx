24bf437a308d84564b0359c97f6404375bd00d4b
uusa35 <uusa35@gmail.com>
2023-01-05 12:03:21 +0300

    add cart footer

--- src/components/AppFooter.tsx
+++ src/components/AppFooter.tsx
@@ -5,9 +5,13 @@ const AppFooter: FC = (): JSX.Element => {
   return (
     <Suspense>
       <footer
-        className={`fixed w-full lg:w-2/4 xl:w-1/3 h-16 left-0 bottom-0 flex flex-row justify-center items-center text-center py-4 border-amber-500 border-2 bg-blue-100`}
+        className={`fixed w-full lg:w-2/4 xl:w-1/3 h-16 left-0 -bottom-1 flex flex-col justify-center items-center text-center py-4 bg-white py-2 shadow-inner shadow-lg`}
       >
-        <h1> {t('powered_by_queue')} &reg;</h1>
+        <div
+          className={`w-full border-b border-stone-100 w-full text-center py-2`}
+        >
+          Cart Here
+        </div>
       </footer>
     </Suspense>
   );
