c3fd8bcb5660c782a6e092a1a51d845bb98932cf
ES484 <esraasaad478@gmail.com>
2023-01-05 20:37:08 +0200

    branchIndex - track order

--- pages/branch/index.tsx
+++ pages/branch/index.tsx
@@ -9,6 +9,13 @@ import { map } from 'lodash';
 import Link from 'next/link';
 import { useAppDispatch } from '@/redux/hooks';
 import { setBranch } from '@/redux/slices/branchSlice';
+import GoogleMapReact from 'google-map-react';
+import Phone from '@/appIcons/phone.svg';
+import Image from 'next/image';
+import { useTranslation } from 'react-i18next';
+import {suppressText, submitBtnClass} from '@/constants/*';
+
+
 
 type Props = {
   elements: Branch[];
@@ -16,18 +23,50 @@ type Props = {
 const BranchIndex: NextPage<Props> = ({ elements }) => {
   console.log('elements', elements);
   const dispatch = useAppDispatch();
+  const {t} = useTranslation();
   return (
     <>
       <MainHead title={`branchIndex`} description={`branchIndex`} />
       <MainContentLayout>
-        <h1>Branch Index</h1>
-        <ul>
-          {map(elements, (b, i) => (
+        <>
+        <h4 className='text-center text-primary_BG font-semibold pt-2'>{t('our_branches')}</h4>
+        {map(elements, (b, i) => (
             <Link href={`#`} onClick={() => dispatch(setBranch(b))} key={i}>
-              {b.name}
+              <p className='font-semibold pb-3' suppressHydrationWarning={suppressText}>{t(b.name)}</p>
+              <div className='w-full h-36 rounded-md'>
+                {/* <GoogleMapReact
+                bootstrapURLKeys={{
+                  // remove the key if you want to fork
+                  key: 'AIzaSyChibV0_W_OlSRJg2GjL8TWVU8CzpRHRAE',
+                  language: 'en',
+                  region: 'US',
+                }}
+                defaultCenter={{
+                  lat: parseInt(b.lat),
+                  lng: parseInt(b.lang),
+                }}
+                defaultZoom={11}
+              >  
+              </GoogleMapReact> */}
+              </div>
+              <div className="flex justify-between my-5 items-center">
+                <p className="text-primary_BG text-lg font-semibold" suppressHydrationWarning={suppressText}>{t(b.location)}</p>
+                <div className="flex rounded-2xl bg-LightGray py-1 px-5 ltr:ml-2 rtl:mr-2">
+                    <Image src={Phone} alt='phone' className="ltr:mr-2 rtl:ml-2 h-5 items-center"/>
+                    <p className="whitespace-nowrap">{b.mobile}</p>
+                </div>
+              </div>
             </Link>
           ))}
-        </ul>
+           <div className='py-2'>
+            <button className={`${submitBtnClass}`}>
+              <div className='flex justify-center items-center'>
+                <p className='text-white px-2' suppressHydrationWarning={suppressText}>{t('send_feedback')}</p>
+              </div>
+            </button>
+          </div>
+        </>
+        
       </MainContentLayout>
     </>
   );
--- pages/order/track.tsx
+++ pages/order/track.tsx
@@ -1,10 +1,56 @@
 import { NextPage } from 'next';
 import MainContentLayout from '@/layouts/MainContentLayout';
+import { useTranslation } from 'react-i18next';
+import {suppressText, inputFieldClass, submitBtnClass} from '@/constants/*';
+
 
 const TrackOrder: NextPage = (): JSX.Element => {
+  const {t} = useTranslation();
   return (
     <MainContentLayout>
-      <h1>Track Order</h1>
+       <div>
+        <h4 className='text-center text-primary_BG font-semibold pt-2' suppressHydrationWarning={suppressText}>{t('track_order')}</h4>
+        <div className="px-5 mb-7">
+            <p className="my-3">{t('check_your_order_status')}</p>
+            <input
+            className={`${inputFieldClass}`}
+            type="text"
+            placeholder={`${t('enter_order_id')}`}
+            suppressHydrationWarning={suppressText}
+            />
+        </div>
+            { 
+                <div>
+                     <div className="pb-2 px-7">
+                        <div className="flex justify-between mt-4">
+                        <p className=" text-btnBG text-primary_BG font-semibold" suppressHydrationWarning={suppressText}>{t("order_id")}</p>
+                        <p>{}</p>
+                        </div>
+
+                        <div className="flex justify-between mt-5">
+                        <p className="text-primary_BG font-semibold" suppressHydrationWarning={suppressText}>{t("estimated_time")}</p>
+                        <p>{}</p>
+                        </div>
+                    </div>
+                    <div className="flex justify-between items-end px-7">
+                        <p className="text-primary_BG font-semibold" suppressHydrationWarning={suppressText}>{t("order_pending")}</p>
+                        <p className="text-primary_BG font-semibold"></p>
+                    </div>
+                    <div className="flex flex-col items-center pt-14">
+                        <p className="mb-4 text-lg" suppressHydrationWarning={suppressText}>{t("delivering_to_your_address")}</p>
+                        <p className="text-lg text-center">{}</p>
+                    </div>
+                    <div className='pt-10'>
+                      <button className={`${submitBtnClass} px-4`}>
+                        <div className='flex justify-between items-center'>
+                          <a href='tel:+' className='text-white px-2' suppressHydrationWarning={suppressText}>{t("call_delivery")}</a>
+                          <a href='tel:+' className='text-White' suppressHydrationWarning={suppressText}>{}</a>
+                        </div>
+                      </button>
+                    </div>
+                </div>
+            }
+        </div>
     </MainContentLayout>
   );
 };
