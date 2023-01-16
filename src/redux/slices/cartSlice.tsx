5d21e70f1875797e5980a6495a6f202de7862287
ES484 <esraasaad478@gmail.com>
2023-01-16 11:25:17 +0200

    cart index total

--- src/redux/slices/cartSlice.tsx
+++ src/redux/slices/cartSlice.tsx
@@ -1,6 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { ProductCart, ClientCart } from '@/types/index';
-import { filter, sum, sumBy } from 'lodash';
+import { filter, sum, sumBy, multiply } from 'lodash';
 
 const initialState: ClientCart = {
   grossTotal: 0,
@@ -47,7 +47,11 @@ export const cartSlice = createSlice({
     increaseCartQty: (state: typeof initialState, action: PayloadAction<ProductCart>)=>{
       const itemIndex = state.items.findIndex((item)=>item.productId === action.payload.productId);
       state.items[itemIndex].totalQty += 1;
-
+      state.items[itemIndex].subTotalPrice = multiply(
+      state.items[itemIndex].totalPrice,
+      state.items[itemIndex].totalQty
+      )
+      state.grossTotal = sum([state.items[itemIndex].subTotalPrice, state.grossTotal])
     },
     decreaseCartQty: (state: typeof initialState, action: PayloadAction<ProductCart>)=>{
       const itemIndex = state.items.findIndex((item)=>item.productId === action.payload.productId);
@@ -58,6 +62,11 @@ export const cartSlice = createSlice({
           const nextCartItems = state.items.filter((item)=>item.productId!==action.payload.productId);
           state.items = nextCartItems;
       }
+      state.items[itemIndex].subTotalPrice = multiply(
+      state.items[itemIndex].totalPrice,
+      state.items[itemIndex].totalQty
+    )
+    state.grossTotal = sum([state.items[itemIndex].subTotalPrice, state.grossTotal])
     },
   },
 });
