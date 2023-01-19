fd5f3323b0f09fdaf5ce0bfcb9424bf7ac6a29fd
eren <e.atef@unityco.io>
2023-01-19 10:17:10 +0200

    QPAGES-11

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -302,7 +302,7 @@ export interface CustomerInfo {
   name: string;
   email: string;
   phone?: string;
-  address: {
+  address?: {
     customer_id: number;
     address_type: number;
     block: string;
