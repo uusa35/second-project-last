2e04f4171b0d40c93e2e0e0f34988df5b8972418
uusa35 <uusa35@gmail.com>
2023-01-21 21:12:57 +0300

    add suspense

--- pages/branch/index.tsx
+++ pages/branch/index.tsx
@@ -11,10 +11,9 @@ import { useAppDispatch } from '@/redux/hooks';
 import { setBranch } from '@/redux/slices/branchSlice';
 import GoogleMapReact from 'google-map-react';
 import Phone from '@/appIcons/phone.svg';
-import Image from 'next/image';
 import { useTranslation } from 'react-i18next';
 import { suppressText, submitBtnClass } from '@/constants/*';
-import { useEffect } from 'react';
+import { useEffect, Suspense } from 'react';
 import { setCurrentModule } from '@/redux/slices/appSettingSlice';
 import CustomImage from '@/components/CustomImage';
 
@@ -34,64 +33,65 @@ const BranchIndex: NextPage<Props> = ({ elements }) => {
     <>
       <MainHead title={`branchIndex`} description={`branchIndex`} />
       <MainContentLayout>
-        <div className={`px-4`}>
-          <h4 className="text-center text-primary_BG font-semibold pt-2">
-            {t('our_branches')}
-          </h4>
-          {map(elements, (b, i) => (
-            <Link href={`#`} onClick={() => dispatch(setBranch(b))} key={i}>
-              <p
-                className="font-semibold pb-3"
-                suppressHydrationWarning={suppressText}
-              >
-                {t(b.name)}
-              </p>
-              <div className="w-full h-36 rounded-md">
-                <GoogleMapReact
-                bootstrapURLKeys={{
-                  key: 'AIzaSyChibV0_W_OlSRJg2GjL8TWVU8CzpRHRAE',
-                  language: 'en',
-                  region: 'US',
-                }}
-                defaultCenter={{
-                  lat: parseInt(b.lat),
-                  lng: parseInt(b.lang),
-                }}
-                defaultZoom={11}
-              >  
-              </GoogleMapReact>
-              </div>
-              <div className="flex justify-between my-5 items-center">
+        <Suspense>
+          <div className={`px-4`}>
+            <h4 className="text-center text-primary_BG font-semibold pt-2">
+              {t('our_branches')}
+            </h4>
+            {map(elements, (b, i) => (
+              <Link href={`#`} onClick={() => dispatch(setBranch(b))} key={i}>
                 <p
-                  className="text-primary_BG text-lg font-semibold"
+                  className="font-semibold pb-3"
                   suppressHydrationWarning={suppressText}
                 >
-                  {t(b.location)}
+                  {t(b.name)}
                 </p>
-                <div className="flex rounded-2xl bg-LightGray py-1 px-5 ltr:ml-2 rtl:mr-2">
-                  <CustomImage
-                    src={Phone}
-                    alt="phone"
-                    className="ltr:mr-2 rtl:ml-2 h-5 items-center"
-                  />
-                  <p className="whitespace-nowrap">{b.mobile}</p>
+                <div className="w-full h-36 rounded-md">
+                  <GoogleMapReact
+                    bootstrapURLKeys={{
+                      key: 'AIzaSyChibV0_W_OlSRJg2GjL8TWVU8CzpRHRAE',
+                      language: 'en',
+                      region: 'US',
+                    }}
+                    defaultCenter={{
+                      lat: parseInt(b.lat),
+                      lng: parseInt(b.lang),
+                    }}
+                    defaultZoom={11}
+                  ></GoogleMapReact>
                 </div>
-              </div>
-            </Link>
-          ))}
-          <div className="py-2">
-            <button className={`${submitBtnClass}`}>
-              <div className="flex justify-center items-center">
-                <p
-                  className="text-white px-2"
-                  suppressHydrationWarning={suppressText}
-                >
-                  {t('send_feedback')}
-                </p>
-              </div>
-            </button>
+                <div className="flex justify-between my-5 items-center">
+                  <p
+                    className="text-primary_BG text-lg font-semibold"
+                    suppressHydrationWarning={suppressText}
+                  >
+                    {t(b.location)}
+                  </p>
+                  <div className="flex rounded-2xl bg-LightGray py-1 px-5 ltr:ml-2 rtl:mr-2">
+                    <CustomImage
+                      src={Phone}
+                      alt="phone"
+                      className="ltr:mr-2 rtl:ml-2 h-5 items-center"
+                    />
+                    <p className="whitespace-nowrap">{b.mobile}</p>
+                  </div>
+                </div>
+              </Link>
+            ))}
+            <div className="py-2">
+              <button className={`${submitBtnClass}`}>
+                <div className="flex justify-center items-center">
+                  <p
+                    className="text-white px-2"
+                    suppressHydrationWarning={suppressText}
+                  >
+                    {t('send_feedback')}
+                  </p>
+                </div>
+              </button>
+            </div>
           </div>
-        </div>
+        </Suspense>
       </MainContentLayout>
     </>
   );
--- pages/vendor/show/index.tsx
+++ pages/vendor/show/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, Suspense } from 'react';
 import MainContentLayout from '@/layouts/MainContentLayout';
 import { NextPage } from 'next';
 import { Vendor } from '@/types/index';
@@ -15,11 +15,11 @@ import CashOnDelivery from '@/appImages/cash_on_delivery.jpg';
 import Visa from '@/appImages/visa.png';
 import Link from 'next/link';
 import { useTranslation } from 'react-i18next';
-import { suppressText, submitBtnClass } from '@/constants/*';
-import { 
-  setCurrentModule, 
+import { suppressText } from '@/constants/*';
+import {
+  setCurrentModule,
   resetShowFooterElement,
-  setShowFooterElement 
+  setShowFooterElement,
 } from '@/redux/slices/appSettingSlice';
 import { useAppDispatch } from '@/redux/hooks';
 import CustomImage from '@/components/CustomImage';
@@ -71,90 +71,95 @@ const VendorShow: NextPage<Props> = ({ element }) => {
         mainImage={element.logo}
       />
       <MainContentLayout>
-        <VendorDetailsItem
-          icon={
-            <CustomImage
-              src={Clock}
-              width={20}
-              height={20}
-              alt={t('work_hours')}
-              className={`w-6 h-6`}
-            />
-          }
-          text="work_hours"
-          content={element.WorkHours}
-        />
-        <VendorDetailsItem
-          icon={
-            <CustomImage
-              src={DeliveryIcon}
-              width={22}
-              height={22}
-              alt={t('delivery_time')}
-              className={`w-6 h-6`}
-            />
-          }
-          text="delivery_time"
-          content={element.DeliveryTime}
-        />
-        <VendorDetailsItem
-          icon={
-            <CustomImage
-              src={PreOrderAvailabilityIcon}
-              width={25}
-              height={25}
-              alt={t('preorder_availability')}
-              className={`w-6 h-6`}
-            />
-          }
-          text="preorder_availability"
-          content={element.Preorder_availability}
-        />
-        <div className="px-4 py-6 shadow-md">
-          <div className="flex justify-between pb-20 ps-3">
-            <div className="flex items-center">
+        <Suspense>
+          <VendorDetailsItem
+            icon={
               <CustomImage
-                src={PaymentIcon}
-                width={25}
-                height={25}
-                alt={t('payment_methods')}
+                src={Clock}
+                width={20}
+                height={20}
+                alt={t('work_hours')}
                 className={`w-6 h-6`}
               />
-              <p
-                className="px-2 font-semibold"
-                suppressHydrationWarning={suppressText}
-              >
-                {t('payment_methods')}
-              </p>
-            </div>
-            <div className="flex items-center">
-              {element.Payment_Methods.visa && (
-                <Link href={'/'} className="px-5">
-                  <CustomImage
-                    src={Visa.src}
-                    className="h-10 w-12"
-                    alt={t('visa')}
-                  />
-                </Link>
-              )}
-              {element.Payment_Methods.cash_on_delivery && (
-                <Link href={'/'} className="px-5">
-                  <CustomImage
-                    className="h-10 w-12"
-                    src={CashOnDelivery.src}
-                    alt={t('cash_on_delivery')}
-                  />
-                </Link>
-              )}
-              {element.Payment_Methods.knet && (
-                <Link href={'/'} className="px-5">
-                  <CustomImage className="h-8 w-12" src={Knet.src} alt="knet" />
-                </Link>
-              )}
+            }
+            text="work_hours"
+            content={element.WorkHours}
+          />
+          <VendorDetailsItem
+            icon={
+              <CustomImage
+                src={DeliveryIcon}
+                width={22}
+                height={22}
+                alt={t('delivery_time')}
+                className={`w-6 h-6`}
+              />
+            }
+            text="delivery_time"
+            content={element.DeliveryTime}
+          />
+          <VendorDetailsItem
+            icon={
+              <CustomImage
+                src={PreOrderAvailabilityIcon}
+                width={25}
+                height={25}
+                alt={t('preorder_availability')}
+                className={`w-6 h-6`}
+              />
+            }
+            text="preorder_availability"
+            content={element.Preorder_availability}
+          />
+          <div className="px-4 py-6 shadow-md">
+            <div className="flex justify-between pb-20 ps-3">
+              <div className="flex items-center">
+                <CustomImage
+                  src={PaymentIcon}
+                  width={25}
+                  height={25}
+                  alt={t('payment_methods')}
+                  className={`w-6 h-6`}
+                />
+                <p
+                  className="px-2 font-semibold"
+                  suppressHydrationWarning={suppressText}
+                >
+                  {t('payment_methods')}
+                </p>
+              </div>
+              <div className="flex items-center">
+                {element.Payment_Methods.visa && (
+                  <Link href={'/'} className="px-5">
+                    <CustomImage
+                      src={Visa.src}
+                      className="h-10 w-12"
+                      alt={t('visa')}
+                    />
+                  </Link>
+                )}
+                {element.Payment_Methods.cash_on_delivery && (
+                  <Link href={'/'} className="px-5">
+                    <CustomImage
+                      className="h-10 w-12"
+                      src={CashOnDelivery.src}
+                      alt={t('cash_on_delivery')}
+                    />
+                  </Link>
+                )}
+                {element.Payment_Methods.knet && (
+                  <Link href={'/'} className="px-5">
+                    <CustomImage
+                      className="h-8 w-12"
+                      src={Knet.src}
+                      alt="knet"
+                    />
+                  </Link>
+                )}
+              </div>
             </div>
           </div>
-          
-        </div>
+        </Suspense>
       </MainContentLayout>
     </>
   );
