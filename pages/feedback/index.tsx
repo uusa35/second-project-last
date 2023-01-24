a663f013176b6bb83b88e9e47f8b5a342b84d165
Esraa.Saad <Esraa.Saad>
2023-01-24 19:14:16 +0200

    feedback

--- pages/feedback/index.tsx
+++ pages/feedback/index.tsx
@@ -7,12 +7,15 @@ import Card from '@/appIcons/card.svg';
 import Comment from '@/appIcons/comment.svg';
 import Phone from '@/appIcons/phone.svg';
 import CustomImage from '@/components/CustomImage';
-import { useAppSelector } from '@/redux/hooks';
+import { useAppDispatch, useAppSelector } from '@/redux/hooks';
 import { useCreateFeedbackMutation } from '@/redux/api/feedbackApi';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import { submitBtnClass, suppressText } from '@/constants/*';
 import { useForm } from 'react-hook-form';
+import { debounce, map } from 'lodash';
+import { useState } from 'react';
+import { showToastMessage } from '@/redux/slices/appSettingSlice';
 type Props = {
   isOpen: boolean;
   ariaHideApp: boolean;
@@ -27,15 +30,13 @@ const Feedback: NextPage<Props> = ({
   const {
     locale: { isRTL },
   } = useAppSelector((state) => state);
-  
+  console.log({isRTL})
   const schema = yup
-  .object({
+  .object().shape({
     user_name: yup.string().min(2).max(50).required(),
     rate: yup.number().min(1).max(3).required(),
     note: yup.string().required().min(2).max(50),
-    phone: yup.number().min(100000).max(999999999999),
-  })
-  .required();
+  }).required()
   const {
     register,
     handleSubmit,
@@ -51,8 +52,9 @@ const Feedback: NextPage<Props> = ({
       phone: `` 
     },
   });
-
+  const [rateVal, setRateVal] = useState<number>();
   const [triggerCreateFeedback] = useCreateFeedbackMutation();
+  const dispatch = useAppDispatch();
 
   const onSubmit = async (body: any) => {
     await triggerCreateFeedback(body)
@@ -60,7 +62,13 @@ const Feedback: NextPage<Props> = ({
       console.log({res: r, body})
 
       if(r.data && r.data.status) {
-        console.log({res: r})
+        dispatch(
+          showToastMessage({
+            content: `${t(`thanks_for_your_feedback`)}`,
+            type: `success`,
+          }) 
+        )
+        onRequestClose()
       }
     })
   }
@@ -68,22 +76,22 @@ const Feedback: NextPage<Props> = ({
     <Modal
       isOpen={isOpen}
       ariaHideApp={ariaHideApp}
-      className={`rounded-t-lg h-1/4 w-full ${
-        isRTL ? `right-0` : `left-0`
-      }`}
+      className={`rounded-t-lg h-1/4 ${isRTL ? 'right-0': 'left-0'}`}
       style={{ overlay: { backgroundColor: 'rgba(0, 0, 0, 0.5)' } }}
     >
       <div
-        className={`bg-white rounded-t-lg bottom-0 absolute w-full lg:w-2/6`}
+        className={`bg-white rounded-t-lg bottom-0 absolute w-full lg:w-2/6 ${isRTL ? ' right-0': 'left-0'}`}
       >
-        <div className="flex justify-between items-center px-5 py-4 w-full ">
+        <div className={`flex justify-between items-center px-5 py-4 w-full ${
+        isRTL && `flex-row-reverse`
+      }`}>
           <div className="flex items-center">
             <CustomImage
               className="w-5 h-5"
               src={ModalFeedbackIcon}
               alt={t('feedback')}
             />
-            <p className="ps-4 capitalize text-primary_BG fontsemibold">
+            <p className="ps-4 capitalize text-primary_BG fontsemibold" suppressHydrationWarning={suppressText}>
               {t('leave_feedback')}
             </p>
           </div>
@@ -97,53 +105,76 @@ const Feedback: NextPage<Props> = ({
         </div>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="px-4">
-          <div className='flex'>
-            <button onClick={()=>setValue('rate', 1)}>
+        <div className={`flex justify-between w-[80%] m-auto pb-4 ${isRTL && `flex-row-reverse`}`}>
+            <button className={`border-zinc-400 border-2 px-2 rounded-full py-1 text-zinc-400 
+              ${rateVal === 1 && 'bg-primary_BG text-white border-zinc-50'}`} 
+              suppressHydrationWarning={suppressText}
+              onClick={()=> {
+                setValue('rate', 1);
+                setRateVal(1);
+              }}>
                 {t('can_be_better')}
             </button>
-            <button onClick={()=>setValue('rate', 2)}>
+            <button className={`border-zinc-400 border-2 px-2 rounded-full py-1 text-zinc-400 
+              ${rateVal === 2 && 'bg-primary_BG text-white border-zinc-50'}`}
+              suppressHydrationWarning={suppressText}  
+              onClick={()=> {
+              setValue('rate', 2);
+              setRateVal(2);
+            }}>
                 {t('it_was_okay')}
             </button>
-            <button onClick={()=>setValue('rate', 3)}>
-                {t('amazing')}
-            </button>
-            {errors?.rate?.message && (
-            <p
-              className={`text-base text-red-800 font-semibold py-2 capitalize`}
+            <button className={`border-zinc-400 border-2 px-2 rounded-full py-1 text-zinc-400 
+              ${rateVal === 3 && 'bg-primary_BG text-white border-zinc-50'}`}  
               suppressHydrationWarning={suppressText}
-            >
-              {t('rate_is_required')}
-            </p>
-          )}          </div>
-          <div className="flex text-black">
+              onClick={()=> {
+              setValue('rate', 3);
+              setRateVal(3);
+            }}>
+                {t('amazing')}
+            </button>         
+        </div>
+        <div>
+          {errors?.rate?.message && (
+              <p
+                className={`text-base text-red-800 font-semibold py-2 capitalize`}
+                suppressHydrationWarning={suppressText}
+              >
+                {t('rate_is_required')}
+              </p>
+            )} 
+        </div>
+          <div className={`flex text-black py-4 ${isRTL && `flex-row-reverse`}`}>
             <Image src={Card} alt="card" width={20} height={20} />
             <input
               {...register('name')}
-              className={`border-0 focus:ring-transparent outline-none capitalize`}
-              type="text"
+              className={`px-4 border-0 focus:ring-transparent outline-none ${isRTL && 'text-right'}`}
               placeholder={`${t(`enter_your_name`)}`}
               onChange={(e) => setValue('user_name', e.target.value)}
               aria-invalid={errors.user_name ? 'true' : 'false'}
             />
           </div>
-          {errors?.user_name?.message && (
-            <p
-              className={`text-base text-red-800 font-semibold py-2 capitalize`}
-              suppressHydrationWarning={suppressText}
-            >
-              {t('name_is_required')}
-            </p>
-          )}
+          <div>
+            {errors?.user_name?.message && (
+              <p
+                className={`text-base text-red-800 font-semibold py-2 capitalize`}
+                suppressHydrationWarning={suppressText}
+              >
+                {t('name_is_required')}
+              </p>
+            )}
+          </div>
           <div className="my-2 px-5 py-1 bg-gray-100"></div>
-          <div className="flex justify-between">
-            <div className="flex text-black">
+          <div className={`flex justify-between py-4 ${isRTL && `flex-row-reverse`}`}>
+            <div className={`flex text-black py-4 ${isRTL && `flex-row-reverse`}`}>
               <Image src={Phone} alt="phone" width={20} height={20} />
               <input
-                className={`border-0 focus:ring-transparent outline-none capitalize`}
-                type="text"
+                className={`px-4 border-0 focus:ring-transparent outline-none ${isRTL && 'text-right'}`}
                 {...register('phone')}
                 onChange={(e) => setValue('phone', e.target.value)}
+                aria-invalid={errors.phone ? 'true' : 'false'}
                 placeholder={`${t(`enter_your_phone`)}`}
+                suppressHydrationWarning={suppressText}
               />
             </div>
             <p className="text-primary_BG text-base capitalize">
@@ -152,15 +183,16 @@ const Feedback: NextPage<Props> = ({
           </div>
           <div className="my-2 px-5 py-1 bg-gray-100"></div>
 
-          <div className="flex text-black">
+          <div className={`flex text-black py-4 ${isRTL && `flex-row-reverse`}`}>
             <Image src={Comment} alt="comment" width={20} height={20} />
             <input
               {...register('note')}
               aria-invalid={errors.note ? 'true' : 'false'}
-              className={`border-0 focus:ring-transparent outline-none capitalize`}
+              className={`px-4 border-0 focus:ring-transparent outline-none ${isRTL && 'text-right'}`}
               type="text"
               placeholder={`${t(`say_something_about_us`)}`}
               onChange={(e) => setValue('note', e.target.value)}
+              suppressHydrationWarning={suppressText}
             />
           </div>
           <div>
@@ -175,7 +207,6 @@ const Feedback: NextPage<Props> = ({
           </div>
         </div>
 
-        {/* buttons */}
         <div className="px-5 pb-5">
           <button className={`w-full capitalize ${submitBtnClass}`}>
             {t('send_feedback')}
