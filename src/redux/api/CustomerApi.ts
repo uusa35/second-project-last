192d3205b0984092f7df4df60851336503a50c6b
Esraa.Saad <Esraa.Saad>
2023-01-24 13:56:13 +0200

    disable yup on pickup cart address

--- src/redux/api/CustomerApi.ts
+++ src/redux/api/CustomerApi.ts
@@ -1,19 +1,45 @@
 import { apiSlice } from './index';
-import { AppQueryResult, Branch } from '@/types/queries';
-import { CustomerInfo, Locale } from '@/types/index';
+import { Address, AppQueryResult } from '@/types/queries';
+import { Prefrences } from '@/types/index';
 
-export const customerApi = apiSlice.injectEndpoints({
+export const addressApi = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
-    SaveCustomerInfo: builder.mutation<
-      AppQueryResult<any>,
+    createAddress: builder.mutation<
+      AppQueryResult<Address>,
       {
-        body: CustomerInfo;
+        body: {
+          address_type: number | string;
+          longitude: number | string;
+          latitude: number | string;
+          customer_id: number | string;
+          address: { [key: string]: any };
+        };
       }
     >({
       query: ({ body }) => ({
-        url: `customer-info`,
+        url: `add-address`,
         method: `POST`,
         body,
+        validateStatus: (response, result) =>
+          response.status === 200 && result.status,
+      }),
+    }),
+
+    checkTimeAvilability: builder.mutation<
+      AppQueryResult<any>,
+      {
+        params: Prefrences;
+        process_type: string;
+        area_branch:string
+      }
+    >({
+      query: ({ params ,process_type,area_branch}) => ({
+        url: `checkAvailableTime`,
+        params: { ...params },
+        headers:{
+          ...(process_type === 'delivery' && {'x-area-id': area_branch}),
+          ...(process_type === 'pickup' && {'x-branch-id': area_branch})
+        },
         validateStatus: (response, result) =>
           response.status === 200 && result.status,
       }),
@@ -21,4 +47,5 @@ export const customerApi = apiSlice.injectEndpoints({
   }),
 });
 
-export const { useSaveCustomerInfoMutation } = customerApi;
+export const { useCreateAddressMutation, useCheckTimeAvilabilityMutation } =
+  addressApi;
