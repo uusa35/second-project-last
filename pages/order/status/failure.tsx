a0ac228276c8b93eb664c9d859da61a9357f5a14
uusa35 <uusa35@gmail.com>
2023-01-30 11:02:01 +0300

    input search height

--- pages/order/status/failure.tsx
+++ pages/order/status/failure.tsx
@@ -16,6 +16,7 @@ import CustomImage from '@/components/CustomImage';
 import { useEffect } from 'react';
 import { setCurrentModule } from '@/redux/slices/appSettingSlice';
 import { useGetCartProductsQuery } from '@/redux/api/cartApi';
+import { themeColor } from '@/redux/slices/vendorSlice';
 const OrderFailure: NextPage = (): JSX.Element => {
   const { t } = useTranslation();
   const {
@@ -23,6 +24,7 @@ const OrderFailure: NextPage = (): JSX.Element => {
     area: { id: areaId },
     customer: { userAgent },
   } = useAppSelector((state) => state);
+  const color = useAppSelector(themeColor);
   const { data: cartItems, isSuccess } = useGetCartProductsQuery({
     UserAgent: userAgent,
   });
@@ -37,6 +39,7 @@ const OrderFailure: NextPage = (): JSX.Element => {
           <CustomImage
             src={Failure.src}
             alt={`${t('failure')}`}
+            className={`w-22 h-auto`}
             width={imageSizes.sm}
             height={imageSizes.sm}
           />
@@ -44,7 +47,7 @@ const OrderFailure: NextPage = (): JSX.Element => {
             className="text-red-700 font-semibold py-3"
             suppressHydrationWarning={suppressText}
           >
-            {t('we_re_sorry')}
+            {t('we_r_sorry')}
           </h4>
           <p suppressHydrationWarning={suppressText}>
             {t('your_order_placement_is_failed')}
@@ -62,20 +65,28 @@ const OrderFailure: NextPage = (): JSX.Element => {
               )}
             </p>
           </div>
+          {isSuccess && cartItems.data?.Cart?.length > 0 && (
+            <Link
+              href={appLinks.cartIndex.path}
+              className={`${submitBtnClass} flex items-center justify-center`}
+              suppressHydrationWarning={suppressText}
+              style={{ backgroundColor: color }}
+            >
+              <div className="bg-red-600 rounded-full w-6 h-6 flex items-center justify-center border-2 border-white text-white">
+                <p className={`text-white`}>{cartItems.data?.Cart?.length}</p>
+              </div>
+              <ShoppingBagOutlinedIcon className="w-6 h-6" />
+              <p className="pt-1">{t('my_cart')}</p>
+            </Link>
+          )}
           <Link
-            href={appLinks.cartIndex.path}
+            href={appLinks.productSearchIndex(branchId, ``, areaId)}
             className={`${submitBtnClass} flex items-center justify-center`}
             suppressHydrationWarning={suppressText}
+            style={{ backgroundColor: color }}
           >
-            <div className="bg-CustomRed rounded-full w-6 h-6 flex items-center justify-center border-2 border-white">
-              <p>{cartItems.data?.Cart?.length}</p>
-            </div>
-            <ShoppingBagOutlinedIcon className="w-6 h-6" />
-            <p className="pt-1">{t('my_cart')}</p>
-          </Link>
-          <Link href={appLinks.productSearchIndex(branchId, ``, areaId)}>
             <p
-              className={`${submitBtnClass} text-center`}
+              className={`text-center text-white`}
               suppressHydrationWarning={suppressText}
             >
               {t('retry_order')}
