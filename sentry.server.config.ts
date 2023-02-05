29e7c9bd791994a6a9f628047db88d97db66c7f2
uusa35 <uusa35@gmail.com>
2023-02-05 17:33:28 +0300

    add edge sentary

---
+++ sentry.server.config.ts
@@ -0,0 +1,13 @@
+import * as Sentry from '@sentry/nextjs';
+import { isLocal } from './src/constants';
+
+Sentry.init({
+  dsn: isLocal
+    ? process.env.NEXT_PUBLIC_SENTRY_DEV
+    : process.env.NEXT_PUBLIC_SENTRY_DEV,
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
