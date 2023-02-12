7f748cb853df081d32c54939f9f814d4f73e0caa
uusa35 <uusa35@gmail.com>
2023-02-12 14:58:55 +0300

    updates

--- pages/order/[orderId]/invoice.tsx
+++ pages/order/[orderId]/invoice.tsx
@@ -11,7 +11,11 @@ import { AppQueryResult } from '@/types/queries';
 import { OrderInvoice } from '@/types/index';
 import { apiSlice } from '@/redux/api';
 import { useEffect, Suspense } from 'react';
-import { setCurrentModule, setUrl } from '@/redux/slices/appSettingSlice';
+import {
+  setCurrentModule,
+  setShowFooterElement,
+  setUrl,
+} from '@/redux/slices/appSettingSlice';
 import { themeColor } from '@/redux/slices/vendorSlice';
 
 type Props = {
@@ -29,6 +33,7 @@ const OrderInvoice: NextPage<Props> = ({ element, url }): JSX.Element => {
 
   useEffect(() => {
     dispatch(setCurrentModule('invoice'));
+    dispatch(setShowFooterElement(`home`));
     if (url) {
       dispatch(setUrl(url));
     }
