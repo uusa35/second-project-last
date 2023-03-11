b237eae8f300b61caf119b384480af6c44d5baf5
uusa35 <uusa35@gmail.com>
2023-03-11 18:51:20 +0300

    from local

--- pages/category/[categoryId]/[method]/[elementId]/[[...params]].tsx
+++ pages/category/[categoryId]/[method]/[elementId]/[[...params]].tsx
@@ -228,7 +228,7 @@ const ProductIndex: NextPage<Props> = ({
               !isNull(searchKey) && currentProducts.length < 3
                 ? `h-min`
                 : `h-[100vh]`
-            }  overflow-y-scroll
+            }  
               ${
                 productPreview === 'hor'
                   ? `grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-2 gap-x-3 py-4`
