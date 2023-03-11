f8404d3f43c1fbc6af6be0af30135658e1f5ab07
Esraa.Saad <Esraa.Saad>
2023-03-11 17:28:04 +0200

    fix price on selection alignment

--- src/components/widgets/product/HorProductWidget.tsx
+++ src/components/widgets/product/HorProductWidget.tsx
@@ -103,14 +103,16 @@ const HorProductWidget: FC<Props> = ({
                 </div>
               ) : (
                 <p
-                  className="text-md text-end uppercase"
+                  className="text-md uppercase"
                   suppressHydrationWarning={suppressText}
                   style={{ color: `black` }}
                 >
                   {parseFloat(element.price).toFixed(3) === '0.000'
                   ? t(`price_on_selection`)
                   : parseFloat(element.price).toFixed(3)} 
-                      <span className={`uppercase`}>{t(`kwd`)}</span>
+                  {parseFloat(element.price).toFixed(3) !== '0.000' && (
+                    <span className={`uppercase px-1`}>{t('kwd')}</span>
+                  )}
                     </p>
               )}
               {/* <p
