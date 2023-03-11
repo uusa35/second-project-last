79f9f6535082b6b20d9bdf332d3bae6481e4dc00
uusa35 <uusa35@gmail.com>
2023-03-11 19:11:47 +0300

    merge with eren

--- pages/category/[categoryId]/[[...params]].tsx
+++ pages/category/[categoryId]/[[...params]].tsx
@@ -217,7 +217,7 @@ const ProductIndex: NextPage<Props> = ({
           <div
             ref={listRef}
             onScroll={onScroll}
-            className={` ${scrollClass}
+            className={`
               ${
                 productPreview === 'hor'
                   ? `grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-2 gap-x-3 py-4`
