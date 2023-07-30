0047a6cede719a30edb237049c4f8764466511b7
uusa35 <uusa35@gmail.com>
2023-07-30 07:29:15 +0300

    updates

--- src/components/home/HomeVendorMainInfo.tsx
+++ src/components/home/HomeVendorMainInfo.tsx
@@ -49,8 +49,9 @@ const HomeVendorMainInfo: FC<Props> = ({ url }): JSX.Element => {
   }, [branch_id, area_id, url]);
 
   if (!isSuccess || !element || !element.Data) return <></>;
-  //  /_next/image?url=https%3A%2F%2Fqueue-spaces.nyc3.digitaloceanspaces.com%2Fvendor_1%2Flogo%2F178%2FWhatsApp-Image-2023-06-30-at-10.19.18-PM.jpeg&w=256&q=75
-  // https://logmaline.getq.me/_next/image?url=https%3A%2F%2Fmybusiness.getq.me%2Fdefault%2FnotFound.png&w=256&q=75
+
+  console.log('element', element.Data);
+
   return (
     <>
       <div className="flex gap-x-2 justify-between items-start capitalize">
@@ -90,6 +91,7 @@ const HomeVendorMainInfo: FC<Props> = ({ url }): JSX.Element => {
                   <div
                     className={`w-4 h-4 rounded-full ${currentStoreStatus[0].className} rtl:ml-2 ltr:mr-2`}
                   ></div>
+
                   <p suppressHydrationWarning={suppressText}>
                     ({t(currentStoreStatus[0].status)})
                   </p>
@@ -98,14 +100,19 @@ const HomeVendorMainInfo: FC<Props> = ({ url }): JSX.Element => {
             </div>
             {/* payment info */}
             <div className="text-sm text-neutral-400 space-y-1">
-              <p suppressHydrationWarning={suppressText}>
-                <Check className="text-lime-400 text-base checkCircle" />
-                {t('payment_by_cards')}
-              </p>
-              <p suppressHydrationWarning={suppressText}>
-                <Check className="text-lime-400 text-base checkCircle" />
-                {t('cash_on_delivery')}
-              </p>
+              {element.Data.Payment_Methods.knet ||
+              element.Data.Payment_Methods.visa ? (
+                <p suppressHydrationWarning={suppressText}>
+                  <Check className="text-lime-400 text-base checkCircle" />
+                  {t('payment_by_cards')}
+                </p>
+              ) : null}
+              {element.Data.Payment_Methods.cash_on_delivery && (
+                <p suppressHydrationWarning={suppressText}>
+                  <Check className="text-lime-400 text-base checkCircle" />
+                  {t('cash_on_delivery')}
+                </p>
+              )}
             </div>
           </div>
         </div>
