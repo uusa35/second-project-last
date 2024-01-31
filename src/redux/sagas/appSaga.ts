9d3b3e767b7077401ce3e90547d1a0cb787a4c25
uusa35 <uusa35@gmail.com>
2024-01-31 14:04:19 +0300

    localstorage fixes

--- src/redux/sagas/appSaga.ts
+++ src/redux/sagas/appSaga.ts
@@ -4,21 +4,17 @@ import i18n from 'i18next';
 import { toast, TypeOptions } from 'react-toastify';
 import { appSettingSlice } from '@/redux/slices/appSettingSlice';
 import { lowerCase, snakeCase } from 'lodash';
-import { persistor } from '@/redux/store';
 
+import { persistor } from '@/redux/store';
 
 export function* startResetEnireAppSceanrio() {
   const version: any = localStorage.getItem('version');
-  if (process.env.NEXT_PUBLIC_APP_VERSION) {
-    if (version && process.env.NEXT_PUBLIC_APP_VERSION && version !== process.env.NEXT_PUBLIC_APP_VERSION) {
-      localStorage.setItem('version', process.env.NEXT_PUBLIC_APP_VERSION);
-      yield delay(5000)
-      persistor.purge()
-      yield delay(5000)
-      window.location.reload();
-    } else {
-      localStorage.setItem('version', process.env.NEXT_PUBLIC_APP_VERSION);
-    }
+  if (process.env.NEXT_PUBLIC_APP_VERSION && version !== process.env.NEXT_PUBLIC_APP_VERSION) {
+    localStorage.setItem('version', process.env.NEXT_PUBLIC_APP_VERSION);
+    yield delay(5000)
+    persistor.purge()
+    yield delay(5000)
+    window.location.reload();
   }
 }
 
